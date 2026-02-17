0a0a71f30b3c4b61a65e1930f25d27bdf93938bf
Poori Charicha <phuri.c@kkumail.com>
2026-02-17 13:56:24 +0700

    Implement review functionality with CRUD operations and validation

---
+++ code/backend/src/routes/review.routes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const reviewController = require('../controllers/review.controller');
+const validate = require('../middlewares/validate');
+const { protect } = require('../middlewares/auth');
+const { createReviewSchema, editReviewSchema} = require('../validations/review.validation');
+const upload = require('../middlewares/upload.middleware');
+
+const router = express.Router();
+
+// GET /api/reviews/me/:userId
+router.get(
+    '/me',
+    protect,
+    reviewController.getReviewsForUser
+);
+
+// POST /api/reviews
+router.post(
+    '/',
+    protect,
+    upload.array('files', 3),    
+    validate(createReviewSchema),
+    reviewController.createReview
+);
+// DELETE /api/reviews/:reviewId
+router.delete(
+    '/:reviewId',
+    protect,
+    reviewController.deleteReview
+);
+
+// PUT /api/reviews/:reviewId
+router.put(
+    '/:reviewId',
+    protect,
+    upload.array('files', 3),
+    validate(editReviewSchema),
+    reviewController.editReview
+);
+module.exports = router;
