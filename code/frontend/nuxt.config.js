6b76c0934632b4affb42a1e643e715e940afb37b
Poori Charicha <phuri.c@kkumail.com>
2026-03-01 11:00:16 +0700

    Update environment configuration to use dynamic API base URL

--- code/frontend/nuxt.config.js
+++ code/frontend/nuxt.config.js
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
-      apiBase: "https://backend-se.pasitlab.com/api/",
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://localhost:3000/api/",
       // apiBase: "http://localhost:3000/api/",
       // apiBase:"https://painamnae-backend.onrender.com/api/",
       googleMapsApiKey: process.env.NUXT_PUBLIC_GOOGLE_MAPS_API_KEY || "",
